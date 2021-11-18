513af993b46cbc6b48a1562d664b18f31f576a2b
Connor Burns <connor.burns@mail.utoronto.ca>
2021-11-18 14:34:04 -0500

    server working

--- server.js
+++ server.js
@@ -28,16 +28,6 @@ app.get("/problem", (req, res) => {
   //res.status(867).send('There was a problem on the server')
 });
 
-app.get("/pub", (req, res) => {
-  // You can indicate a status code to send back
-  // by default it is 200, but it's up to you
-  // if you want to send something
-  res.status(500).redirect("/pub/test.html");
-
-  // don't send nonsense status codes like this one:
-  //res.status(867).send('There was a problem on the server')
-});
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   log(`Listening on port ${port}...`);
